e73272e9008347e6f8a89d445fd37f173defa106
Mnabawy <m_ninja61@yahoo.com>
2022-01-16 17:13:11 +0200

    initial

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { StyleSheet, Text, View, FlatList } from "react-native"
+
+const ProductOverviewScreen = () => {
+  return (
+    <FlatList>
+      <Text></Text>
+    </FlatList>
+  )
+}
+
+export default ProductOverviewScreen
+
+const styles = StyleSheet.create({})
