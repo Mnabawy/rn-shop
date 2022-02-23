f034b6a7e21346f3a32ce6b56888ff04b37e92d8
Mnabawy <m_ninja61@yahoo.com>
2022-02-23 10:32:54 +0200

    login and error handled

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,10 +1,11 @@
-import React, { useReducer } from "react";
+import React, { useState, useCallback, useReducer, useEffect } from "react";
 import {
   StyleSheet,
   ScrollView,
   Button,
   View,
-  KeyboardAvoidingView,
+  ActivityIndicator,
+  Alert,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import { useDispatch } from "react-redux";
@@ -13,7 +14,6 @@ import Card from "../../components/ui/Card";
 import Input from "../../components/ui/Input";
 import Colors from "../../constants/Colors";
 import * as authActions from "../../store/actions/auth";
-import { useCallback } from "react";
 
 const FORM_INPUT_UPDATE = "FORM_INPUT_UPDATE ";
 
@@ -42,6 +42,9 @@ const formReducer = (state, action) => {
 };
 
 const AuthScreen = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSignedUp, SetIsSignedUp] = useState(false);
+  const [error, setError] = useState();
   const dispatch = useDispatch();
 
   const [formState, dispatchFormState] = useReducer(formReducer, {
@@ -68,66 +71,87 @@ const AuthScreen = () => {
     [dispatchFormState]
   );
 
-  const signupHandler = () => {
-    dispatch(
-      authActions.signup(
+  useEffect(() => {
+    if (error) {
+      Alert.alert("An Error Occurred!", error, [{ text: "Okay" }]);
+    }
+  }, [error]);
+
+  const authHandler = async () => {
+    let action;
+    if (isSignedUp) {
+      action = authActions.signup(
         formState.inputValues.email,
         formState.inputValues.password
-      )
-    );
+      );
+    } else {
+      action = authActions.login(
+        formState.inputValues.email,
+        formState.inputValues.password
+      );
+    }
+    setError(null);
+
+    setIsLoading(true);
+    try {
+      await dispatch(action);
+    } catch (err) {
+      setError(err.message);
+    }
+    setIsLoading(false);
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior="padding"
-      keyboardVerticalOffset={50}
-      style={styles.screen}
-    >
-      <LinearGradient colors={["#ffedff", "#ffe3ff"]} style={styles.gradient}>
-        <Card style={styles.authContainer}>
-          <ScrollView>
-            <Input
-              id="email"
-              label="E-Mail"
-              keyboardType="email-address"
-              required
-              email
-              autoCapitalize="none"
-              errorText="Please enter a valid email address"
-              value={formState.inputValues.email}
-              onInputChange={inputChangeHandler}
-              initialValue=""
+    <LinearGradient colors={["#ffedff", "#ffe3ff"]} style={styles.gradient}>
+      <Card style={styles.authContainer}>
+        <ScrollView>
+          <Input
+            id="email"
+            label="E-Mail"
+            keyboardType="email-address"
+            required
+            email
+            autoCapitalize="none"
+            errorText="Please enter a valid email address"
+            value={formState.inputValues.email}
+            onInputChange={inputChangeHandler}
+            initialValue=""
+          />
+          <Input
+            id="password"
+            label="Password"
+            keyboardType="default"
+            required
+            minLength={5}
+            autoCapitalize="none"
+            errorText="Please enter a valid password"
+            value={formState.inputValues.password}
+            onInputChange={inputChangeHandler}
+            initialValue=""
+          />
+          <View style={styles.btnContainer}>
+            <Button
+              title={isSignedUp ? "Sign Up" : "Login"}
+              color={Colors.primary}
+              onPress={authHandler}
             />
-            <Input
-              id="password"
-              label="Password"
-              keyboardType="default"
-              required
-              minLength={5}
-              autoCapitalize="none"
-              errorText="Please enter a valid password"
-              value={formState.inputValues.password}
-              onInputChange={inputChangeHandler}
-              initialValue=""
-            />
-            <View style={styles.btnContainer}>
+          </View>
+          <View style={styles.btnContainer}>
+            {isLoading ? (
+              <ActivityIndicator size="small" color={Colors.primary} />
+            ) : (
               <Button
-                title="Login"
-                color={Colors.primary}
-                onPress={signupHandler}
-              />
-            </View>
-            <View style={styles.btnContainer}>
-              <Button
-                title="Switch To Sign Up"
+                title={`swtich To ${isSignedUp ? "Login" : "Sign Up"}`}
                 color={Colors.accint}
-                onPress={() => {}}
+                onPress={() => {
+                  SetIsSignedUp(prevState => !prevState);
+                }}
               />
-            </View>
-          </ScrollView>
-        </Card>
-      </LinearGradient>
-    </KeyboardAvoidingView>
+            )}
+          </View>
+        </ScrollView>
+      </Card>
+    </LinearGradient>
   );
 };
 
